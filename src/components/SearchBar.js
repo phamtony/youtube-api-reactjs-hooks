9603f4810ff11069dbd4b13f4deb9e0bc84afc19
Tony Pham <tony.p@lashowroom.com>
2020-12-16 19:31:01 -0800

    setting up search bar functions. getting ready to tie it up to the app js and youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    onInputChange = (e) => {
+        this.setState({term: e.target.value});
+    };
+
     render() {
         return (
-          <div>
-              Search bar
-          </div>
+            <div className="search-bar ui segment">
+                <form action="" className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label htmlFor="">Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
         );
     }
 }

b4e0265e7d5f189185a13b9913935215bdacf846
Tony Pham <tony.p@lashowroom.com>
2020-12-16 19:38:22 -0800

    install axios and set up youtube api

---
+++ src/api/Youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const KEY = 'AIzaSyAs48Caon5nk2WS5e0UMcyI_KvpLcpZOFQ';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+});
